a23fa2664a8ff0202f783b9d996987774f597ac2
Haddadson <gabriel.haddad15@gmail.com>
2018-05-25 22:15:16 -0300

    changes file's tree

--- assets/js/alveus-tech.js
+++ assets/js/alveus-tech.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	//Inicia o banco de dados assim que a página é carregada
 	initDBEngine();
 	openDB();
-
+	console.log("C ta logado ? " + isLogged);
 	//Adiciona Listeners
 
 	// Intercepta o click do botão Logar
@@ -14,7 +14,9 @@ $(document).ready(function(){
 
                 logar(usuarioLogin, function (usuario) {
                 	console.log(usuario);
-
+                	if(usuario != null){
+                		var isLogged = true;
+                	}
                 });
                 // $("#form-login")[0].reset();
                 
